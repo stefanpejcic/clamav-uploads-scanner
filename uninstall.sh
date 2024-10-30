018e125807015564a6f00f82bc77b6393c206a88
Stefan Pejcic <stefan@pejcic.rs>
2024-10-30 14:33:02 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -45,9 +45,23 @@ remove_service() {
 }
 
 
+remove_files(){
+    scanner_dir="/usr/local/clamav-uploads-scanner"
+    echo "Deleting $scanner_dir" 
+    rm -rf $scanner_dir
+}
+
+remove_monitoring(){
+    echo "Removing service from OpenAdmin > Services Status" 
+    service_to_remove="clamav_monitor"
+    services_file="/etc/openpanel/openadmin/config/services.json"
+    jq "del(.[] | select(.real_name == \"$service_to_remove\"))" "$services_file" > tmp.$$.json && mv tmp.$$.json "$services_file"
+    echo "Service '$service_to_remove' removed from $services_file."
+}
+
+
 remove_service
-scanner_dir="/usr/local/clamav-uploads-scanner"
-echo "Deleting $scanner_dir" 
-rm -rf $scanner_dir
+remove_files
+remove_monitoring
 
 echo "DONE"
